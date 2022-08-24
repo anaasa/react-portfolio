052ebfb108df758989b0383a5cec6ea3b80b7286
Ana Stoica <ananastoica@gmail.com>
2022-08-24 17:50:47 +0300

    Module 8

--- src/components/portfolio/portfolio-container.js
+++ src/components/portfolio/portfolio-container.js
@@ -1,65 +1,95 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import axios from "axios";
 
-import PortfolioItem from './portfolio-item';
+import PortfolioItem from "./portfolio-item";
 
 export default class PortfolioContainer extends Component {
+  constructor() {
+    super();
 
-    constructor() {
-        super();
-        
-        this.state = {
-            pageTitle: "Welcome to my portfolio",
-            data: [
-              { title: "Quip", category: "eCommerce" },
-              { title: "Eventbrite", category: "Scheduling" },
-              { title: "Ministry Safe", category: "Enterprise" },
-              { title: "SwingAway", category: "eCommerce" }
-            ]
-        };
+    this.state = {
+      pageTitle: "Welcome to my portfolio",
+      isLoading: false,
+      data: []
+    };
 
-        console.log("Portfolio container has rendered");
+    this.handleFilter = this.handleFilter.bind(this);
+  }
 
-        this.handleFilter = this.handleFilter.bind(this);
+  handleFilter(filter) {
+    if (filter === "CLEAR_FILTERS") {
+      this.getPortfolioItems();
+    } else {
+      this.getPortfolioItems(filter);
+    }
+  }
 
+  getPortfolioItems(filter = null) {
+    axios
+      .get("https://anastoica.devcamp.space/portfolio/portfolio_items")
+      .then(response => {
+        if (filter) {
+          this.setState({
+            data: response.data.portfolio_items.filter(item => {
+              return item.category === filter;
+            })
+          });
+        } else {
+          this.setState({
+            data: response.data.portfolio_items
+          });
+        }
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }
+
+  portfolioItems() {
+    return this.state.data.map(item => {
+      return <PortfolioItem key={item.id} item={item} />;
+    });
+  }
+
+  componentDidMount() {
+    this.getPortfolioItems();
+  }
+
+  render() {
+    if (this.state.isLoading) {
+      return <div>Loading...</div>;
     }
 
-    handleFilter(filter) {
-        this.setState({
-          data: this.state.data.filter(item => {
-            return item.category === filter;
-          })
-        });
-    }
-
-    portfolioItems() {
-    
-        return this.state.data.map(item => {
-          return <PortfolioItem title={item.title} url={"google.com"} />;
-        });
-    }
-
-    render() {
-        return (
-            <div>
-                <h2>{this.state.pageTitle}</h2>
-
-                <button onClick={() => this.handleFilter("eCommerce")}>
-                    eCommerce
-                </button>
-                <button onClick={() => this.handleFilter("Scheduling")}>
-                    Scheduling
-                </button>
-                <button onClick={() => this.handleFilter("Enterprise")}>
-                    Enterprise
-                </button>
-
-                {this.portfolioItems ()}
-
-
-            </div>
-
-            
-        );
-    }
+    return (
+      <div className="homepage-wrapper">
+        <div className="filter-links">
+          <button
+            className="btn"
+            onClick={() => this.handleFilter("eCommerce")}
+          >
+            eCommerce
+          </button>
+          <button
+            className="btn"
+            onClick={() => this.handleFilter("Scheduling")}
+          >
+            Scheduling
+          </button>
+          <button
+            className="btn"
+            onClick={() => this.handleFilter("Enterprise")}
+          >
+            Enterprise
+          </button>
+          <button
+            className="btn"
+            onClick={() => this.handleFilter("CLEAR_FILTERS")}
+          >
+            All
+          </button>
+        </div>
+        <div className="portfolio-items-wrapper">{this.portfolioItems()}</div>
+      </div>
+    );
+  }
 }
-
